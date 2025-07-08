1edea21ac6ec1a9c138b2b0f8040b38ce9935e33
Emre Altınok <emre0altinok@gmail.com>
2025-07-08 11:57:41 +0300

    HP UI ve Animasyon Güncellemesi
    HP Bar için script yazıldı.
    Animasyonda bazı hızlar ve ayarlar değiştirildi.
    UI ekranına geçici can barı eklendi.

--- Assets/##Assets/Scripts/AnaKarakterCanSistemi.cs
+++ Assets/##Assets/Scripts/AnaKarakterCanSistemi.cs
@@ -1,6 +1,58 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class AnaKarakterCanSistemi
+public class AnaKarakterCanSistemi : MonoBehaviour
 {
-    
+    [SerializeField] private float can = 100; 
+    public float Can => can; 
+    public float maksimumCan = 100; 
+    public Image canBar;
+
+    private PlayerController playerController;
+
+    private void Awake()
+    {
+        playerController = GetComponent<PlayerController>();
+    }
+
+    public void CanAzalt(float miktar)
+    {
+        can -= miktar;
+        if (can < 0)
+        {
+            can = 0;
+            Olum();
+        }
+        Debug.Log("Can azaldý: " + can);
+    }
+
+    public void CanArttir(float miktar)
+    {
+        can += miktar;
+        if (can > maksimumCan)
+        {
+            can = maksimumCan;
+        }
+        Debug.Log("Can arttý: " + can);
+    }
+
+
+    private void Olum()
+    {
+        // Ölüm iþlemleri -
+        Debug.Log("Karakter öldü!");
+        playerController.Die(); 
+        //animatörü kapat
+
+    }
+
+
+    private void Update()
+    {
+        if (canBar != null)
+        {
+            canBar.fillAmount = can / maksimumCan;
+        }
+    }
 }
+
